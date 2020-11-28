b78b3217d085ac522b070af22ac9edc435a52d64
abhishek upadhyay <abhishekupadhyay483@gmail.com>
2020-11-28 07:42:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express=require('express');
+const app=express();
+const port=8001;
+app.use('/',require('/routes'));
+app.listen(port,function(error){
+ if(error){
+          console.log('error!!!',error)
+ }
+ console.log('my server is running on the port=',port);
+})
+
